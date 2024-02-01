588d2fb1f7944f7a7d15e0915b2f7f3945320382
[Georgi Karchev] <georgikarcev@gmail.com>
2024-01-31 23:44:53 -0500

    Create a multi-page React application using BrowserRouter

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react"
+//import "./App.css";
+
+// Import Route and our components
+import { Route, Routes } from "react-router-dom";
+import Currencies from "./pages/Currencies";
+import Main from "./pages/Main";
+import Price from "./pages/Price";
+
+//import components
+import Nav from "./components/Nav";
+
+export default function App () {
+  return (
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main/>}/>
+        <Route path="/currencies" element={<Currencies/>}/>
+        <Route path="/price" element={<Price/>}/>
+      </Routes>
+    </div>
+  );
+}
