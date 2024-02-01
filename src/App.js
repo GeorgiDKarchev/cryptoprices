51d6f4b950519744f027c6d1b868e92215fc36a1
[Georgi Karchev] <georgikarcev@gmail.com>
2024-02-01 00:18:58 -0500

    Adding Route price/:simbol

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ export default function App () {
         <Route path="/" element={<Main/>}/>
         <Route path="/currencies" element={<Currencies/>}/>
         <Route path="/price" element={<Price/>}/>
+        <Route path="/price/:symbol" element={<Price />} />
       </Routes>
     </div>
   );
